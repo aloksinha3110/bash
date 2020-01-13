e34e6f26ead4da32fea3ce057c42030163c6f46e
aloksinha3110 <59638727+aloksinha3110@users.noreply.github.com>
2020-01-13 10:32:05 +0530

    logic change

--- ping.sh
+++ ping.sh
@@ -2,44 +2,33 @@
 
 HOSTS="graph.facebook.com"
 COUNT=3
-ETC_HOSTS="/etc/hosts"
+ETC_HOSTS=/etc/hosts
 
 for myHost in $HOSTS
 do
-  count=$(ping -c $COUNT $myHost | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
-  ip=$(ping -c 1 graph.facebook.com | gawk -F'[()]' '/PING/{print $2}')
-#if [ $count -eq 3 ]; then
-   echo "Host : $myHost with IP Address $ip is up (ping is comming) at $(date)"
- if [ $count -eq 0 ]; then
-    #addip to etc/hosts
-    echo "Host : $myHost with IP Address $ip is down (ping failed) at $(date)"
-    echo "Adding Facebook Graph IP address to your $ETC_HOSTS";
-     if [ -n "$(grep $ip /etc/hosts)" ]
-        then
-            echo "IP address already exists IP: $(grep $ip $ETC_HOSTS)"
-        else
-            echo "Adding $ip to your $ETC_HOSTS";
-            sudo -- sh -c -e "echo '$ip' >> /etc/hosts";
+        count=$(ping -c $COUNT $myHost | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
+        ip=$(ping -c 1 graph.facebook.com | gawk -F'[()]' '/PING/{print $2}')
+        echo "Host : $myHost with IP Address $ip is up (ping is comming) at $(date)"
+        matches_in_hosts="$(grep -n $myHost /etc/hosts | cut -f1 -d:)"
+        host_entry="${ip} ${myHost}"
 
-            if [ -n "$(grep $ip /etc/hosts)" ]
-                then
-                    echo "$ip was added succesfully \n $(grep $ip /etc/hosts)";
-                else
-                    echo "Failed to Add $ip, Try again!";
-            fi
+if [ ! -z "$matches_in_hosts" ]
+then
+    echo "Found existing hosts entry."
+  if [ -n "$(grep $myHost /etc/hosts)" ]
+then
+        echo "$myHost Found in your $ETC_HOSTS,...";
+        #sudo sed -i".bak" "/$myHost/d" $ETC_HOSTS
+    else
+        echo "$myHost was not found in your $ETC_HOSTS";
     fi
+    while read -r line_number; do
+        #sudo sed -i '' "${line_number}s/.*/${host_entry} /" /etc/hosts
+        echo "Nothing is going to update";
+    done <<< "$matches_in_hosts"
+else
+    echo "Adding new hosts entry for $myHost with IP Address $ip."
+    echo "$host_entry" | sudo tee -a /etc/hosts > /dev/null
 fi
-        #echo "Host : $myHost with IP Address $ip is down (ping failed) at $(date)"
-#fi
-if [ $count -gt 3 ]; then
-#remove() {
-   if [ -n "$(grep -P "[[:space:]]$ip" /etc/hosts)" ]; then
-       echo "$ip found in $ETC_HOSTS. Removing now...";
-       try sudo sed -ie "/[[:space:]]$ip/d" "$ETC_HOSTS";
-   else
-       Your "$ip was not found in $ETC_HOSTS";
-   fi
-#}
-fi
-#echo "Ping is comming from IP" $ip;
+
 done
